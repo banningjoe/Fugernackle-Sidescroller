73ca62844b5b0c666d04de90f2b9b8b41c8ddcf3
banningjoe <banningjoe@gmail.com>
2017-08-25 01:42:09 -0400

    Began building class heirarchy structure, learned that it is possible to make instances of the outermost class in a script, so I should remake the structure and rename the scripts so that I don't have the 'class' ending

---
+++ Assets/Game Assets/Heirarchy/Characters/CharacterClass.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterClass : EntityClass {
+	List<GameObject> images;
+	string name;
+	List<string> quotes;
+	TileClass start;
+}
