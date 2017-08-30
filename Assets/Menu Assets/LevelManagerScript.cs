0481003471656a9a821e114c531d3b5ccbcaabfb
banningjoe <banningjoe@gmail.com>
2017-08-30 15:41:00 -0400

    still working on having the online repo reflect the local

---
+++ Assets/Menu Assets/LevelManagerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManagerScript : MonoBehaviour {
+	public void SwitchScene(string scene) {
+		Debug.Log ("Attempting to load \"" + scene + "\" scene");
+		Application.LoadLevel(scene);
+	}
+	public void QuitGame(){
+		Debug.Log ("Attempting to quit game");
+		Application.Quit();
+	}
+}
